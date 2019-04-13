42867b6ce4e6cb79259e62735fb5b779334c09ca
Andrey Shulepov <andrey.shulepov@gmail.com>
2019-04-13 17:09:40 +0500

    graceful workee

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gocron-graceful
 
 go 1.12
+
+require github.com/jasonlvhit/gocron v0.0.0-20190402024347-5bcdd9fcfa9b
---
+++ graceful.go
@@ -0,0 +1,40 @@
+package graceful
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/jasonlvhit/gocron"
+)
+
+// Worker runs task worker
+func Worker(name string, task func(wg *sync.WaitGroup)) {
+	log.Printf("%s: starting", name)
+
+	wg := &sync.WaitGroup{}
+
+	task(wg)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	doneChan := gocron.Start()
+
+	<-signalChan
+
+	log.Printf("%s: shutting down...", name)
+	doneChan <- true
+	wg.Wait()
+
+	log.Printf("%s: done", name)
+}
+
+// TaskWrapper is wrapper for graceful tasks
+func TaskWrapper(wg *sync.WaitGroup, task func()) {
+	wg.Add(1)
+	task()
+	wg.Done()
+}
