0ec856145c65b7a3abdb2a55812962707daf9d0a
Andrey Shulepov <andrey.shulepov@gmail.com>
2019-04-17 18:45:32 +0500

    test TaskWrapper

--- graceful_test.go
+++ graceful_test.go
@@ -25,7 +25,7 @@ func testWorker(t *testing.T, signal syscall.Signal) {
 	time.Sleep(100 * time.Millisecond)
 
 	// Kill Worker
-	if err := syscall.Kill(syscall.Getpid(), syscall.SIGINT); err != nil {
+	if err := syscall.Kill(syscall.Getpid(), signal); err != nil {
 		t.Errorf("kill graceful with '%v' error: %v", signal, err)
 	}
 
@@ -38,4 +38,27 @@ func task(wg *sync.WaitGroup) {
 	go func() {
 		<-cron.Start()
 	}()
+}
+
+func TestTaskWrapper(t *testing.T) {
+	timeout := time.After(3 * time.Second)
+	done := make(chan bool)
+
+	go func(done chan bool) {
+		// run test
+		wg := &sync.WaitGroup{}
+		for i := 0; i < 5; i++ {
+			TaskWrapper(wg, func() { t.Log("task", i) })
+		}
+		wg.Wait()
+
+		// synced on time
+		done <- true
+	}(done)
+
+	select {
+	case <-timeout:
+		t.Fatal("timeout error")
+	case <-done:
+	}
 }
